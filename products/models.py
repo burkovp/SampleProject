17ea3938b5d3ad5210d1859ae263a0ab90d4bfda
burkovp <burkovp@mail.ru>
2023-02-16 00:17:32 +0300

    create models 15022023

--- products/models.py
+++ products/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-# class Details(models.Model):
-#     name = models.CharField(max_length=256)
-#     description = models.TextField(null =True, blank=True)
-#     price = models.DecimalField(max_digits=14, decimal_places=2)
-#     quantity = models.PositiveIntegerField(default=0)
-#     # image = models.ImageField(upload_to='details_images')
+class Details(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null =True, blank=True)
+    price = models.DecimalField(max_digits=14, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    # image = models.ImageField(upload_to='details_images')
